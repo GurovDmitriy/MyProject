ff0990c0c7b24e5eebd42f87a2d8b487a38ba219
GurovDmitriy <gurovdmitriy1991@gmail.com>
2020-07-17 13:05:19 +0300

    Added base animation index page

--- source/js/index.js
+++ source/js/index.js
@@ -1,14 +1,2 @@
-'use strict';
+'use strict'
 
-const burger = document.querySelector('.burger');
-const menu = document.querySelector('.menu');
-
-burger.onclick = function () {
-  if (burger.classList.contains('burger--open')) {
-    burger.classList.toggle('burger--close');
-  } else {
-    burger.classList.add('burger--open');
-  }
-
-  menu.classList.toggle('menu--open');
-}

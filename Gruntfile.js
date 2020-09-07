8264fdb99ccdac1a3320e53afc17e2a9a5a4f6ba
GurovDmitriy <gurovdmitriy1991@gmail.com>
2020-09-07 10:14:39 +0300

    Fix html links

--- Gruntfile.js
+++ Gruntfile.js
@@ -218,6 +218,70 @@ module.exports = function (grunt) {
       },
     },
 
+    realFavicon: {
+      favicons: {
+        src: 'TODO: Path to your master picture',
+        dest: 'TODO: Path to the directory where to store the icons',
+        options: {
+          iconsPath: '/',
+          html: [ 'TODO: List of the HTML files where to inject favicon markups' ],
+          design: {
+            ios: {
+              pictureAspect: 'noChange',
+              assets: {
+                ios6AndPriorIcons: true,
+                ios7AndLaterIcons: true,
+                precomposedIcons: true,
+                declareOnlyDefaultIcon: true
+              }
+            },
+            desktopBrowser: {
+              design: 'raw'
+            },
+            windows: {
+              pictureAspect: 'noChange',
+              backgroundColor: '#da532c',
+              onConflict: 'override',
+              assets: {
+                windows80Ie10Tile: false,
+                windows10Ie11EdgeTiles: {
+                  small: true,
+                  medium: true,
+                  big: true,
+                  rectangle: true
+                }
+              }
+            },
+            androidChrome: {
+              pictureAspect: 'noChange',
+              themeColor: '#ffffff',
+              manifest: {
+                display: 'standalone',
+                orientation: 'notSet',
+                onConflict: 'override',
+                declared: true
+              },
+              assets: {
+                legacyIcon: false,
+                lowResolutionIcons: false
+              }
+            },
+            safariPinnedTab: {
+              pictureAspect: 'silhouette',
+              themeColor: '#5bbad5'
+            }
+          },
+          settings: {
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false,
+            readmeFile: false,
+            htmlCodeFile: true,
+            usePathAsIs: false
+          }
+        }
+      }
+    }
+
     copy: {
       buildCopy: {
         files: [{

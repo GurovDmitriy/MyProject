218b8e6635e147b5f4219208e01bfecbccc3375e
GurovDmitriy <gurovdmitriy1991@gmail.com>
2020-10-05 19:58:02 +0300

    Update build

--- docs/js/default.js
+++ docs/js/default.js
@@ -1,1 +1,27 @@
-"use strict";const burger=document.querySelector(".burger"),menu=document.querySelector(".menu");burger.onclick=function(){burger.classList.contains("burger--open")?burger.classList.toggle("burger--close"):burger.classList.add("burger--open"),menu.classList.toggle("menu--open")};const btnUp=document.querySelector(".btn-up"),headerLogo=document.querySelector(".header__logo");function topFunction(element){document.body.scrollTop=0,document.documentElement.scrollTop=0,element.focus()}btnUp.onclick=function(){topFunction(headerLogo)};
+'use strict'
+
+// burger
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+burger.onclick = function () {
+  if (burger.classList.contains('burger--open')) {
+    burger.classList.toggle('burger--close');
+  } else {
+    burger.classList.add('burger--open');
+  }
+
+  menu.classList.toggle('menu--open');
+}
+
+// btn up
+const btnUp = document.querySelector('.btn-up');
+const headerLogo = document.querySelector('.header__logo');
+
+btnUp.onclick = function() {topFunction(headerLogo)};
+
+function topFunction(element) {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  element.focus();
+}
